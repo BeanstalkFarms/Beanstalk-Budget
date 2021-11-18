5be4e93c558d20e34c4cefcab2db93a00c1ca3be
Beanstalk Farms <88561107+BeanstalkFarms@users.noreply.github.com>
2021-11-17 21:58:06 -0500

    BUP-1: Sowing Beans (#1)
    bup-1
    - adding functionality for the Budget contract to sow
    - add functionality for the Budget contract to pay in the form of a plot.

---
+++ contracts/mock/MockFieldFacet/PodTransfer.sol
@@ -0,0 +1,51 @@
+/**
+ * SPDX-License-Identifier: MIT
+**/
+
+pragma solidity ^0.8.6;
+
+import "./Dibbler.sol";
+
+/**
+ * @author Publius
+ * @title Pod Transfer
+**/
+contract PodTransfer is Dibbler {
+
+    /**
+     * Getters
+    **/
+
+    function allowancePods(address owner, address spender) public view returns (uint256) {
+        return s.a[owner].field.podAllowances[spender];
+    }
+
+    /**
+     * Internal
+    **/
+
+    function insertPlot(address account, uint256 id, uint256 amount) internal {
+        s.a[account].field.plots[id] = amount;
+    }
+
+    function removePlot(address account, uint256 id, uint256 start, uint256 end) internal {
+        uint256 amount = plot(account, id);
+        if (start == 0) delete s.a[account].field.plots[id];
+        else s.a[account].field.plots[id] = start;
+        if (end != amount) s.a[account].field.plots[id + end] = amount - end;
+    }
+
+    function decrementAllowancePods(address owner, address spender, uint256 amount) internal {
+        uint256 currentAllowance = allowancePods(owner, spender);
+        setAllowancePods(
+            owner,
+            spender,
+            currentAllowance - amount
+        );
+    }
+
+    function setAllowancePods(address owner, address spender, uint256 amount) internal {
+        s.a[owner].field.podAllowances[spender] = amount;
+    }
+
+}
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,14 +1,44 @@
 const BEAN_ADDRESS = '0xDC59ac4FeFa32293A95889Dc396682858d52e5Db'
+const BEANSTALK_ADDRESS = '0xC1E088fC1323b20BCBee9bd1B9fC9546db5624C5'
+const DEVELOPMENT_ADDRESS = '0x83A758a6a24FE27312C1f8BDa7F3277993b64783'
+const MARKETING_ADDRESS = '0xAA420e97534aB55637957e868b658193b112A551'
+
+function addCommas (nStr) {
+  nStr += ''
+  const x = nStr.split('.')
+  let x1 = x[0]
+  const x2 = x.length > 1 ? '.' + x[1] : ''
+  var rgx = /(\d+)(\d{3})/
+  while (rgx.test(x1)) {
+    x1 = x1.replace(rgx, '$1' + ',' + '$2')
+  }
+  return x1 + x2
+}
+
+function strDisplay (str) {
+  return addCommas(str.toString())
+}
 
 async function main() {
   const BudgetFactory = await ethers.getContractFactory("Budget");
-  const DevelopmentBudget = await upgrades.deployProxy(BudgetFactory, [BEAN_ADDRESS]);
-  await DevelopmentBudget.deployed();
-  console.log("Development Budget deployed to:", DevelopmentBudget.address);
+  const budget = await upgrades.deployProxy(BudgetFactory, [BEAN_ADDRESS, BEANSTALK_ADDRESS]);
+  console.log(budget.deployTransaction);
+  const receipt = await budget.deployTransaction.wait();
+  console.log("Budget deployed to:", budget.address);
+  console.log(`Gas used: ${strDisplay(receipt.gasUsed)}`);
+}
 
-  const MarketingBudget = await upgrades.deployProxy(BudgetFactory, [BEAN_ADDRESS]);
-  await MarketingBudget.deployed();
-  console.log("Marketing Budget deployed to:", MarketingBudget.address);
+async function upgrade(account) {
+  const BudgetFactory = await ethers.getContractFactory("Budget");
+  budget = await upgrades.upgradeProxy(MARKETING_ADDRESS, BudgetFactory);
+  console.log("Marketing Address Upgraded deployed to:", MARKETING_ADDRESS);
+  await (await budget.setBeanstalk(BEANSTALK_ADDRESS)).wait()
+  console.log("Beanstalk Address set to ", BEANSTALK_ADDRESS);
+
+  budget = await upgrades.upgradeProxy(DEVELOPMENT_ADDRESS, BudgetFactory);
+  console.log("Marketing Address Upgraded deployed to:", DEVELOPMENT_ADDRESS);
+  await (await budget.setBeanstalk(BEANSTALK_ADDRESS)).wait()
+  console.log("Beanstalk Address set to ", BEANSTALK_ADDRESS);
 }
 
 if (require.main === module) {
@@ -20,3 +50,4 @@ if (require.main === module) {
       })
   }
   exports.deploy = main
+  exports.upgrade = upgrade
